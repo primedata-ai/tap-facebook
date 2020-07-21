284d94326069e462b06c10a54123c0f2e8c42d7a
Paul Aan <weburnittv@gmail.com>
2020-07-21 21:32:37 +0700

    release 7.0.3

---
+++ CHANGELOG.md
@@ -0,0 +1,69 @@
+# Changelog
+
+## 1.9.1
+  * Pass metadata from the catalog to the Transformer to filter out unselected fields [#97](https://github.com/singer-io/tap-facebook/pull/97)
+
+## 1.9.0
+  * Bump API version from `v4` to `v6` [#88](https://github.com/singer-io/tap-facebook/pull/88)
+
+## 1.8.2
+  * Add `video_play_curve_actions` to ads_insights schemas [#80](https://github.com/singer-io/tap-facebook/pull/80)
+
+## 1.8.1
+  * Modifies the sync method of AdCreatives to use the FB Batch endpoint [#73](https://github.com/singer-io/tap-facebook/pull/73)
+
+## 1.8.0
+  * Add the ability to override `RESULT_RETURN_LIMIT` from the config [#71](https://github.com/singer-io/tap-facebook/pull/71)
+  * Add date-windowing for the `adcreative` stream [#71](https://github.com/singer-io/tap-facebook/pull/71)
+
+## 1.7.1
+  * Bump `facebook_business` library to 4.0.5 [#68](https://github.com/singer-io/tap-facebook/pull/68)
+  * Remove deprecated `video_p95_watched_actions` field
+  * Change calls to `remote_read` to use `api_get` per deprecation [#69](https://github.com/singer-io/tap-facebook/pull/69)
+
+## 1.7.0
+  * Replaced `annotated_schema` with Singer `metadata`
+    * Fixed unit tests to also use `metadata`
+  * Added integration tests to CircleCI
+
+## 1.6.0
+  * Add DMA breakdown
+
+## 1.5.12
+  * Bump `facebook_business` library to 3.3.2 (#59)(https://github.com/singer-io/tap-facebook/pull/59)
+
+## 1.5.9
+  * Restore Insights job timeout, as Facebook seems to have stabilized.
+
+## 1.5.8
+  * Reduce Insights job wait to fail faster due to Facebook instability.
+
+## 1.5.7
+  * Bump `facebook_business` library to 3.2.0 [#51](https://github.com/singer-io/tap-facebook/pull/51)
+
+## 1.5.6
+  * Update version of `requests` to `2.20.0` in response to CVE 2018-18074
+
+## 1.5.5
+  * Updates the `should_retry_api_error` function to include the error_subcode 99 when Facebook fails to retrieve Ad data
+
+## 1.5.4
+  * Add retry_pattern annotations to record_preparation function calls so they don't fall victim to Facebook 500's as often [#48](https://github.com/singer-io/tap-facebook/pull/48)
+
+## 1.5.3
+  * Yield records to IncrementalStreams instead of accumulating them [#47](https://github.com/singer-io/tap-facebook/pull/47)
+
+## 1.5.2
+  * Bump `facebook_business` library to 3.0.5
+  * Remove deprecated fields from ad_insights schemas [FB 2/2018](https://developers.facebook.com/docs/graph-api/changelog/breaking-changes#feb2018)
+
+## 1.5.1
+  * Bump Insights job timeout to 120 seconds in an attempt to more closely match Facebook's API behavior.
+
+## 1.5.0
+  * Upgraded the Facebook Python API to version 3.0.4 [#44](https://github.com/singer-io/tap-facebook/pull/44)
+  * Added outbound_clicks to the ads_insights streams
+
+## 1.4.0
+  * Upgraded the Facebook API to version 2.11 [#39](https://github.com/singer-io/tap-facebook/pull/39)
+  * Added a new stream for Ads Insights with a breakdown of "region" [#40](https://github.com/singer-io/tap-facebook/pull/40)
---
+++ tests/unittests/test_tap_facebook.py
@@ -0,0 +1,99 @@
+import itertools
+import unittest
+import pendulum
+import tap_facebook
+
+from tap_facebook import AdsInsights
+from singer.catalog import Catalog, CatalogEntry
+from singer.schema import Schema
+from singer.utils import strftime
+
+class TestAdsInsights(unittest.TestCase):
+
+    def test_insights_start_dates(self):
+        insights = AdsInsights(
+            name='insights',
+            account=None,
+            stream_alias="insights",
+            options={},
+            catalog_entry=CatalogEntry(schema={'properties': {'something': {'type': 'object'}}},
+                                       metadata=[{'breadcrumb': ('properties', 'something'),
+                                                  'metadata': {'selected' : True}}]),
+            state={'bookmarks':{'insights': {'date_start': '2017-01-31'}}})
+        params = list(itertools.islice(insights.job_params(), 5))
+        self.assertEqual(params[0]['time_ranges'],
+                         [{'since': '2017-01-03',
+                           'until': '2017-01-03'}])
+
+        self.assertEqual(params[4]['time_ranges'],
+                         [{'since': '2017-01-07',
+                           'until': '2017-01-07'}])
+
+    def test_insights_job_params_stops(self):
+        start_date = tap_facebook.TODAY.subtract(days=2)
+        insights = AdsInsights(
+            name='insights',
+            account=None,
+            stream_alias="insights",
+            options={},
+            catalog_entry=CatalogEntry(schema={'properties': {'something': {'type': 'object'}}},
+                                       metadata=[{'breadcrumb': ('properties', 'something'),
+                                                  'metadata': {'selected' : True}}]),
+            state={'bookmarks':{'insights': {'date_start': start_date.to_date_string()}}})
+
+        self.assertEqual(31, len(list(insights.job_params())))
+
+
+class TestPrimaryKeyInclusion(unittest.TestCase):
+
+    def test_primary_keys_automatically_included(self):
+        streams = tap_facebook.initialize_streams_for_discovery() # Make this list for the key_properties
+        catalog = tap_facebook.discover_schemas()['streams']
+        for catalog_entry in catalog:
+            streamObject = [stream for stream in streams if stream.name == catalog_entry['stream']][0]
+            key_prop_breadcrumbs = {('properties', x) for x in streamObject.key_properties} # Enumerate the breadcrumbs for key properties
+            for field in catalog_entry['metadata']: # Check that all key properties are automatic inclusion
+                if field['breadcrumb'] in key_prop_breadcrumbs:
+                    self.assertEqual(field['metadata']['inclusion'], 'automatic')
+
+class TestGetStreamsToSync(unittest.TestCase):
+
+
+    def test_getting_streams_to_sync(self):
+        catalog_entry= {
+            'streams': [
+                {
+                    'stream': 'adcreative',
+                    'tap_stream_id': 'adcreative',
+                    'schema': {},
+                    'metadata': [{'breadcrumb': (),
+                                  'metadata': {'selected': True}}]
+                },
+                {
+                    'stream': 'ads',
+                    'tap_stream_id': 'ads',
+                    'schema': {},
+                    'metadata': [{'breadcrumb': (),
+                                  'metadata': {'selected': False}}]
+                }
+            ]
+        }
+
+        catalog = Catalog.from_dict(catalog_entry)
+
+        streams_to_sync = tap_facebook.get_streams_to_sync(None, catalog, None)
+        names_to_sync = [stream.name for stream in streams_to_sync]
+        self.assertEqual(['adcreative'], names_to_sync)
+
+class TestDateTimeParsing(unittest.TestCase):
+
+    def test(self):
+        dt       = '2016-07-07T15:46:48-0400'
+        expected = '2016-07-07T19:46:48.000000Z'
+        self.assertEqual(
+            tap_facebook.transform_datetime_string(dt),
+            expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
